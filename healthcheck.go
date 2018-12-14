a41d72d36d0811dc89e43053c4fcc20a552b8142
Tom Weston <weston.tom@gmail.com>
2018-12-14 14:35:40 +0000

    init

---
+++ healthcheck.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func doEvery(d time.Duration, f func(time.Time)) {
+	for x := range time.Tick(d) {
+		f(x)
+	}
+}
+
+func pingmysql(t time.Time) {
+
+	// Get DB details from ENV
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASS")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+
+	// Open Conenction
+	db, err = sql.Open("mysql", user+":"+pass+"@tcp("+host+":"+port+")/")
+	if err != nil {
+		fmt.Println("[connection failed]", host, "[error]", err.Error())
+	}
+
+	// Ping
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("[ping failed]", host, "[error]", err.Error())
+	}
+	// Go uses odd an odd time formating reference date: https://flaviocopes.com/go-date-time-format/
+	fmt.Println("[success]", "[", t.Format("2006-01-02 15:04:05"), "]", "[", host, "]")
+
+	// Close connection to DB
+	defer db.Close()
+
+}
+
+func main() {
+
+	doEvery(100*time.Millisecond, pingmysql)
+
+}
